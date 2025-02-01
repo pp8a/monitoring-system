f9fe9c12958219ee7726f4a8ddc4b622e003f676
Mikhalchuk Aliaksandr <al.mikhalchuk@astondevs.ru>
2025-02-01 16:08:02 +0300

    Refactor SecurityConfig: extract constants, add access rule for /sensors/search

--- monitor-sensors/src/main/java/com/example/monitorsensors/config/SecurityConfig.java
+++ monitor-sensors/src/main/java/com/example/monitorsensors/config/SecurityConfig.java
@@ -21,11 +21,17 @@ import lombok.RequiredArgsConstructor;
 @EnableMethodSecurity
 public class SecurityConfig {
 	
+	private static final String AUTH_URL = "/auth/**";
 	private static final String ROLE_ADMIN = "ADMINISTRATOR";
     private static final String ROLE_VIEWER = "VIEWER";
     private static final String SENSORS_URL = "/sensors/**";
     private static final String SENSOR_TYPES_URL = "/sensor-types/**";
     private static final String MEASUREMENT_UNITS_URL = "/measurement-units/**";
+    private static final String SENSOR_SEARCH_URL = "/sensors/search";    
+    private static final String SWAGGER_URLS = "/swagger-ui/**";
+    private static final String API_DOCS_URLS = "/v3/api-docs/**";
+    private static final String API_DOCS_YAML = "/v3/api-docs.yaml";
+    
 	
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -33,14 +39,15 @@ public class SecurityConfig {
         .csrf(csrf -> csrf.disable()) // Disabling CSRF for the REST API
         .sessionManagement(session -> session.sessionCreationPolicy(STATELESS)) // without session
         .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/auth/**").permitAll() // without authorization
-            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/v3/api-docs.yaml").permitAll()
+            .requestMatchers(AUTH_URL).permitAll() // without authorization
+            .requestMatchers(SWAGGER_URLS, API_DOCS_URLS, API_DOCS_YAML).permitAll()
             .requestMatchers(HttpMethod.GET, SENSORS_URL).hasAnyRole(ROLE_ADMIN, ROLE_VIEWER) 
             .requestMatchers(HttpMethod.POST, SENSORS_URL).hasRole(ROLE_ADMIN) 
             .requestMatchers(HttpMethod.PUT, SENSORS_URL).hasRole(ROLE_ADMIN) 
             .requestMatchers(HttpMethod.DELETE, SENSORS_URL).hasRole(ROLE_ADMIN)
             .requestMatchers(HttpMethod.GET, SENSOR_TYPES_URL).hasAnyRole(ROLE_ADMIN, ROLE_VIEWER)
             .requestMatchers(HttpMethod.GET, MEASUREMENT_UNITS_URL).hasAnyRole(ROLE_ADMIN, ROLE_VIEWER)
+            .requestMatchers(HttpMethod.GET, SENSOR_SEARCH_URL).hasAnyRole(ROLE_ADMIN, ROLE_VIEWER)
             .anyRequest().authenticated()
         )
         .httpBasic(Customizer.withDefaults()); // (Spring Security 6.1+)
